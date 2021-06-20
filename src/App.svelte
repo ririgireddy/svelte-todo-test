3b087b220ce20ae3efb05d866681d94e8e9843b6
ririgireddy <ririgireddy1@gmail.com>
2021-06-19 20:31:47 -0700

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,55 @@
+<script>
+  import Fa from 'svelte-fa'
+  import { faInfoCircle } from '@fortawesome/free-solid-svg-icons'
+  import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+
+    let newItem = '';
+
+    let todoList = [{text: 'Make to do list', status: true},
+                    {text: 'Walk the dog', status: false},
+                    {text: 'Do my homework', status: false}];
+
+	function addToList() {
+		todoList = [...todoList, {text: newItem, status: false}];
+		newItem = '';
+	}
+
+	function removeFromList(index) {
+		todoList.splice(index, 1)
+		todoList = todoList;
+    }
+</script>
+
+<h1>To-Do List</h1>
+
+<div class="todo">
+
+
+
+<br/>
+{#each todoList as item, index}
+	<input bind:checked={item.status} type="checkbox">
+	<span class:checked={item.status}>{item.text}</span>
+	<span on:click={() => removeFromList(index)}><Fa icon={faTrashAlt} /></span>
+	<br/>
+{/each}
+
+
+<div class="add">
+<input bind:value={newItem} type="text" placeholder="new todo item..">
+<button on:click={addToList}>Add</button>
+</div>
+
+</div>
+
+<div class="info">
+
+<Fa icon={faInfoCircle} /> If you resubmit this page, progress will be lost
+</div>
+
+
+<style>
+	.checked {
+        text-decoration: line-through;
+    }
+</style>
